95acaa991ec7f10e1d952fbcb7425ed5dd56a2ad
Sangjae <hellohj@counpia.com>
2021-04-07 16:43:48 +0900

    챕터4 template mustache 까지 완료

--- src/main/java/org/example/bootaws/sevice/posts/PostsService.java
+++ src/main/java/org/example/bootaws/sevice/posts/PostsService.java
@@ -3,13 +3,16 @@ package org.example.bootaws.sevice.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.bootaws.domain.psots.Posts;
 import org.example.bootaws.domain.psots.PostsRepository;
+import org.example.bootaws.web.dto.PostsListResponseDto;
 import org.example.bootaws.web.dto.PostsResponseDto;
 import org.example.bootaws.web.dto.PostsSaveRequestDto;
 import org.example.bootaws.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -38,5 +41,19 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(findPosts);
+    }
+
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts findPosts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(findPosts);
     }
 }
---
+++ src/main/java/org/example/bootaws/web/IndexController.java
@@ -0,0 +1,36 @@
+package org.example.bootaws.web;
+
+import lombok.RequiredArgsConstructor;
+import org.example.bootaws.sevice.posts.PostsService;
+import org.example.bootaws.web.dto.PostsResponseDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto findPostsDto = postsService.findById(id);
+        model.addAttribute("posts", findPostsDto);
+
+        return "posts-update";
+    }
+}
