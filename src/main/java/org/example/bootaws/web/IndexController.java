0a1ec12cb88e27ac8f155e8675a9053cfa03aa80
Sangjae <hellohj@counpia.com>
2021-04-07 20:23:53 +0900

    챕터5 oauth2 적용 및 로그인 테스트와 권한 체크까지 완료

--- src/main/java/org/example/bootaws/web/IndexController.java
+++ src/main/java/org/example/bootaws/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.bootaws.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.bootaws.config.oauth.dto.SessionUser;
 import org.example.bootaws.sevice.posts.PostsService;
 import org.example.bootaws.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -9,15 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
