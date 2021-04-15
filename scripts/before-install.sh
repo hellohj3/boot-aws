e9d245d90da3eb17f7c64d469e6cbbba360471aa
Sangjae <hellohj@counpia.com>
2021-04-15 10:36:13 +0900

    무중단 배포를위한 쉘스크립트 푸쉬

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -3,7 +3,7 @@
 # I want to make sure that the directory is clean and has nothing left over from
 # previous deployments. The servers auto scale so the directory may or may not
 # exist.
-if [ -d /home/ec2-user/app/step2/zip ]; then
-    rm -rf /home/ec2-user/app/step2/zip
+if [ -d /home/ec2-user/app/step3/zip ]; then
+    rm -rf /home/ec2-user/app/step3/zip
 fi
-mkdir -vp /home/ec2-user/app/step2/zip
+mkdir -vp /home/ec2-user/app/step3/zip
